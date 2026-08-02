5e4000806bb5b204e2ab57a37d6bef49ecd26837
Brennon York <yorkbrennon7@gmail.com>
2026-08-01 20:12:45 -0400

    Updated Python Code for about-me, calculator, and Started my Insurance Quoting Project

---
+++ Projects/Python/Insurance_Quoter/insurance-quoter.py
@@ -0,0 +1,8 @@
+# Brennon York 
+# This will eventually be an Insurance Quoter
+# I will get online Insurance Quotes without needing to start an application
+# It will ask for Name, Age, Smoking Info, and State
+#
+# It will then print out a coverage amount, a monthly estimate for companies
+# It will then recommend me a carrier.
+# I will initally only build in Ethos TruStage, Mutual of Omaha, Transamerica, and Aflac 
