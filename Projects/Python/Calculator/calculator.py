7eb83f8f8a513ddde07744400d90854e8c194827
Brennon York <yorkbrennon7@gmail.com>
2026-08-14 16:00:22 -0400

    Refactored calculator.py with math_helper.py and added more functions.

--- Projects/Python/Calculator/calculator.py
+++ Projects/Python/Calculator/calculator.py
@@ -19,32 +19,52 @@ def main():
     print("=====  PYTHON CALCULATOR V1.0  ======\n")
     while True:
         choice = getFunction()
-        if choice == 1:
-            operation = getOperator()
-            variables = getVariables(2)
-            if operation in ("/", "%"):
-                variables[1] = zeroDivision(variables[1])
-            doOperation(operation, variables)
-        elif choice == 2:
+        if choice == 0:
             print("Thanks for using my calculator!")
             break
+        else:
+            operation = getOperator()
+            if operation in ("sqrt", "flr", "ceil", "abs"):
+                variables = getVariables(1)
+                if operation == "sqrt":
+                    variables[0] = sortInput(variables[0], operation)
+            else:
+                variables = getVariables(2)
+                if operation in ("/", "%"):
+                    variables[1] = sortInput(variables[1], operation)
+            doOperation(operation, variables)
 
 def getFunction():
     print("=====================================")
     print("Available Functions:")
-    print("1. Operation")
-    print("2. Quit")
+    print("0. Quit Program")
+    print("1. Basic Operations")
     print("=====================================\n")
     while True:
         function = input("Select a function: ")
-        if function in ("1", "2"):
+        if function in ("0", "1"):
             return int(function)
         print("Please select a valid function.")
 
 def getOperator():
     while True:
-        operation = input("Enter operation (+, -, *, /, %, **): ")
-        if operation not in ('+', '-', '*', '/', '%', '**'):
+        print("=====================================")
+        print("Operations:")
+        print("-Add (+)")
+        print("-Subtract (-)")
+        print("-Multiplication (*)")
+        print("-Divide (/)")
+        print("-Remainder (%)")
+        print("-Power (**)")
+        print("-Square Root (sqrt)")
+        print("-Nth Root (root)")
+        print("-Floor (flr)")
+        print("-Ceil (ceil)")
+        print("-Absolute Value (abs)")
+        print("=====================================\n")
+        operation = input("Select an operation: ").strip()
+        if operation not in ('+', '-', '*', '/', '%', '**', 'sqrt', 'root', 'flr',
+                             'ceil', 'abs'):
             print("Please select a valid operation.")
         else:
             return operation
@@ -61,35 +81,67 @@ def getVariables(amount):
                 print(f"Please enter a valid number for number {i + 1}.")
     return numbers 
 
-def zeroDivision(number):
-    while number == 0:
-        try:
-            number = float(input("Please enter a non-zero number for division: "))
-        except ValueError:
-            print("Please enter a valid number.")
+def sortInput(number, operation):
+    if operation in ("/", "%"):
+        while number == 0:
+            try:
+                number = float(input("Please enter a non-zero number for division: "))
+            except ValueError:
+                print("Please enter a valid number.")
+    elif operation == "sqrt":
+        while number < 0:
+            try:
+                number = float(input("Please enter a non-negative number for sqrt: "))
+            except ValueError:
+                print("Please enter a valid number.")
     return number
 
 def doOperation(operation, variables):
-    num1 = variables[0]
-    num2 = variables[1]
-    if operation == "+":
-        answer = math_helpers.addition(num1, num2)
-    elif operation == "-":
-        answer = math_helpers.subtraction(num1, num2)
-    elif operation == "*":
-        answer = math_helpers.multiplication(num1, num2)
-    elif operation == "/":
-        answer = math_helpers.division(num1, num2)
-    elif operation == "%":
-        answer = math_helpers.remainder(num1, num2)
-    elif operation == "**":
-        answer = math_helpers.power(num1, num2)
-    else:
-        return
-    print("=====================================")
-    print(f"{num1} {operation} {num2} = {answer}")
-    print("=====================================\n")
-
+    if len(variables) == 1:
+        num1 = variables[0]
+        if operation == "sqrt":
+            answer = math_helpers.sqrtRoot(num1)
+        elif operation == "flr":
+            answer = math_helpers.flr(num1)
+        elif operation == "ceil":
+            answer = math_helpers.ceiling(num1)
+        elif operation == "abs":
+            answer = math_helpers.absVal(num1)
+        else:
+            return
+        print("=====================================")
+        if operation in ('flr', 'ceil'):
+            print(f"{num1} {operation}'d is {answer}")
+        elif operation in ('sqrt', 'abs'):
+            print(f"{operation} value of {num1} is {answer}")
+        else:
+            return
+        print("=====================================\n")
+    else: 
+        num1 = variables[0]
+        num2 = variables[1]
+        if operation == "+":
+            answer = math_helpers.addition(num1, num2)
+        elif operation == "-":
+            answer = math_helpers.subtraction(num1, num2)
+        elif operation == "*":
+            answer = math_helpers.multiplication(num1, num2)
+        elif operation == "/":
+            answer = math_helpers.division(num1, num2)
+        elif operation == "%":
+            answer = math_helpers.remainder(num1, num2)
+        elif operation == "**":
+            answer = math_helpers.power(num1, num2)
+        elif operation == "root":
+            answer = math_helpers.nthroot(num1, num2)
+        else:
+            return
+        print("=====================================")
+        if operation in ('+', '-', '*', '/', '%', '**'):
+            print(f"{num1} {operation} {num2} = {answer}")
+        else:
+            print(f"The {num2}th Root of {num1} = {answer}")
+        print("=====================================\n")
 
 # Performing Main
 main()
--- Projects/Python/Calculator/math_helpers.py
+++ Projects/Python/Calculator/math_helpers.py
@@ -1,6 +1,24 @@
 # Brennon York  |  Math Helpers v1.1  |  8/13/2026
 
-# Basic Calculator Functions
+from math import sqrt, floor, ceil 
+
+# -- Basic Calculator Functions --
+## Functions with One Variable:
+
+def sqrtRoot(num1):
+    return sqrt(num1)
+
+def flr(num1):
+    return floor(num1)
+
+def ceiling(num1):
+    return ceil(num1)
+
+def absVal(num1):
+    return abs(num1)
+
+
+## Functions with Two or More Variables:
 def addition(num1, num2):
     return num1 + num2
 
@@ -18,3 +36,6 @@ def remainder(num1, num2):
 
 def power(num1, num2):
     return num1 ** num2
+
+def nthroot(num1, num2):
+    return num1 ** (1/num2)
