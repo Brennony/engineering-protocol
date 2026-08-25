8bb6fbc4fbe3012f9cb87ff2f418ef15e957de59
Brennon York <yorkbrennon7@gmail.com>
2026-08-24 21:09:12 -0400

    Started adding Trig Helper (no changes to calculator.py yet).

---
+++ Projects/Python/Calculator/trig_helpers.py
@@ -0,0 +1,51 @@
+# Brennon York  |  Trigonometry Helpers v1  |  8/24/2026
+
+import math
+
+# -- Trig Functions -- 
+
+
+## Convert degrees to radians
+
+def degRads(x):
+    radians = x * (math.pi / 180)
+    return radians
+
+## One Variable Trig
+
+def sine(x):
+    return math.sin(x)
+
+def cosine(x):
+    return math.cos(x)
+
+def tang(x):
+    if cosine(x) == 0:
+        return None
+    return math.tan(x)
+
+def csc(x):
+    if sine(x) == 0:
+        return None
+    return (1/sine(x))
+
+def sec(x):
+    if cosine(x) == 0:
+        return None
+    return (1/cosine(x))
+
+def cot(x):
+    return (1/tang(x))
+
+def arcsin(x):
+    if -1 > x or 1 < x:
+        return None
+    return math.asin(x)
+
+def arccos(x):
+    if -1 > x or 1 < x:
+        return None
+    return math.acos(x)
+
+def arctan(x):
+    return math.atan(x)
